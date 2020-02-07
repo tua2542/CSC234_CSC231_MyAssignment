487172d4bdb0df1b03c267d1022608df097f965e
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-02-07 12:30:23 +0700

    [ADD] Flutter_Assignment3_Adding Interactivity

---
+++ flutter_assignment_3/lib/main.dart
@@ -0,0 +1,362 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+// TapboxA manages its own state.
+
+//------------------------- TapboxA ----------------------------------
+// class TapboxA extends StatefulWidget {
+//   TapboxA({Key key}) : super(key: key);
+
+//   @override
+//   _TapboxAState createState() => _TapboxAState();
+// }
+
+// class _TapboxAState extends State<TapboxA> {
+//   bool _active = false;
+
+//   void _handleTap() {
+//     setState(() {
+//       _active = !_active;
+//     });
+//   }
+
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: _handleTap,
+//       child: Container(
+//         child: Center(
+//           child: Text(
+//             _active ? 'Active' : 'Inactive',
+//             style: TextStyle(fontSize: 32.0, color: Colors.white),
+//           ),
+//         ),
+//         width: 200.0,
+//         height: 200.0,
+//         decoration: BoxDecoration(
+//           color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//------------------------- MyApp ----------------------------------
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Flutter Demo'),
+//         ),
+//         body: Center(
+//           child: TapboxA(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// ParentWidget manages the state for TapboxB.
+
+//------------------------ ParentWidget --------------------------------
+// class ParentWidget extends StatefulWidget {
+//   @override
+//   _ParentWidgetState createState() => _ParentWidgetState();
+// }
+
+// class _ParentWidgetState extends State<ParentWidget> {
+//   bool _active = false;
+
+//   void _handleTapboxChanged(bool newValue) {
+//     setState(() {
+//       _active = newValue;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: TapboxB(
+//         active: _active,
+//         onChanged: _handleTapboxChanged,
+//       ),
+//     );
+//   }
+// }
+
+// //------------------------- TapboxB ----------------------------------
+// class TapboxB extends StatelessWidget {
+//     TapboxB({Key key, this.active:false, @required this.onChanged})
+//     : super(key: key);
+
+//     final bool active;
+//     final ValueChanged<bool> onChanged;
+
+//     void _handleTap() {
+//       onChanged(!active);
+//     }
+
+//     Widget build(BuildContext context) {
+//       return GestureDetector(
+//         onTap: _handleTap,
+//         child: Container(
+//           child: Center(
+//             child: Text(
+//               active: ? 'Active' : 'Inactive',
+//               style: TextStyle(fontSize: 32.0, color: Colors.white),
+
+//             ),
+//             ),
+//             width: 200.0,
+//             height: 200.0,
+//             decoration: BoxDecoration(
+//               color: active ? Colors.lightGreen[700] : Colors.grey[600],
+//             ),
+//           ),
+//       );
+//     }
+// }
+
+//---------------------------- ParentWidget ----------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//----------------------------- TapboxC ------------------------------
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state.
+    return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          //1
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //2
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+//3
+        FavoriteWidget(),
+      ],
+    ),
+  );
+  Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ));
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'SHARE'),
+        ],
+      ),
+    );
+    return MaterialApp(
+      title: 'Flutter  demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter  demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisSize: MainAxisSize.min, children: [
+      Container(
+        padding: const EdgeInsets.all(0),
+        child: IconButton(
+          icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+          color: Colors.red[500],
+          onPressed: _toggleFavorite,
+        ),
+      ),
+      SizedBox(
+        width: 18,
+        child: Container(
+          child: Text('$_favoriteCount'),
+        ),
+      )
+    ]);
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+  // ···
+}
